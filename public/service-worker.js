28b2749bbcaa3c7e71897fe2bcf0f663e5d9e849
Edwin Troya <edtroyajr@gmail.com>
2021-03-21 15:46:43 -0700

    touched up the J.S

--- public/service-worker.js
+++ public/service-worker.js
@@ -38,4 +38,32 @@ self.addEventListener("activate",function (event){
     self.clients.claim();
 })
 
+//fetch
+self.addEventListener("fetch",function(event){
+    if (event.request.url.includes("/api/")){
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then (cache=>{
+                return fetch(event.request)
+                .then(response=>{
+                    if (response.status === 200){
+                        cache.put(event.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(event.request);
+                })
+            }
+        ).catch(err => console.log(err))
+        );
+    return;
+}
 
+event.respondWith(
+    caches.open(CACHE_NAME).then(cache =>{
+        return cache.match(event.request).then(response=>{
+            return response || fetch(event.request);
+        })
+    })
+)
+});
